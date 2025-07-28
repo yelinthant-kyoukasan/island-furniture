fd52359016854fa5e7a400ff707a000a2d065de1
yelinthant-kyoukasan <jclinton931@gmail.com>
2025-07-28 14:48:47 +0800

    database user changes

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "clinton",
+            password: "root1234",
             database: "islandfurniture-it07",
             multipleStatements: true
         });
@@ -16,7 +16,7 @@ const setting = {
     connectionLimit : 10, //set limit to 10 connection
     host     : 'localhost', 
     user     : 'root', 
-    password : 'clinton', 
+    password : 'root1234', 
     database : 'islandfurniture-it07',
     multipleStatements: true, //allow multiple sql statements
     dateStrings: true, //return date as string instead of Date object
